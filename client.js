e17664f89b60fe9e79947ce307618cfb457a01df
Nirmal Rijal <nirmalrijal41@gmail.com>
2018-12-31 11:25:45 +0545

    Server client communication

--- client.js
+++ client.js
@@ -1,17 +1,40 @@
 var net = require("net");
 
+const readline = require("readline");
+
+const readMove = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const blank = "\n".repeat(process.stdout.rows);
+
+let SERVER_ADDRESS = "127.0.0.1";
+let SERVER_PORT = 1337;
+
 var client = new net.Socket();
 client.connect(
-  1337,
-  "127.0.0.1",
+  SERVER_PORT,
+  SERVER_ADDRESS,
   function() {
     console.log("Connected");
-    client.write("Hello, server! Love, Client.");
+    chooseTheMove();
   }
 );
 
+function chooseTheMove() {
+  readMove.question(`\nWhat's your move : `, move => {
+    handleTheMove(move);
+  });
+}
+
+function handleTheMove(data) {
+  client.write(data);
+}
+
 client.on("data", function(data) {
-  console.log("Received: " + data);
+  const strData = data.toString();
+  console.log(strData);
+  chooseTheMove();
 });
 
 client.on("close", function() {
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var net = require("net");
 
+const connectedSockets = new Set();
+
+connectedSockets.broadcast = function(data, client) {
+  for (let sock of this) {
+    if (sock === client) {
+      sock.write(data);
+    }
+  }
+};
+
 var server = net.createServer(function(socket) {
-  socket.write("Echo server\r\n");
-  console.log(server.getConnections());
-  socket.pipe(socket);
+  if (Number(connectedSockets.size) === 2) {
+    socket.end("Maximum connections");
+  } else {
+    console.log("new client connected");
+    connectedSockets.add(socket);
+    console.log(connectedSockets[0]);
+
+    socket.on("end", function() {
+      connectedSockets.delete(socket);
+    });
+
+    socket.on("data", function(data) {
+      const strData = data.toString();
+      console.log(strData);
+      connectedSockets.broadcast(strData, socket);
+    });
+  }
 });
 
-server.listen(1337, "127.0.0.1");
+const PORT = process.env.PORT || 1337;
+server.listen(PORT);
