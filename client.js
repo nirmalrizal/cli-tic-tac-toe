51ff7973c5377b771e1ce02e98709ad8a8e62436
Nirmal Rijal <nirmalrijal41@gmail.com>
2018-12-31 17:23:57 +0545

    Player name change and wait for game to start

--- client.js
+++ client.js
@@ -1,12 +1,24 @@
 var net = require("net");
-
 const readline = require("readline");
+
+/* Constants */
+const NAME_CHANGE = "NAME_CHANGE";
+const START_GAME = "START_GAME";
+const GAME_MESSAGE = "GAME_MESSAGE";
 
 const readMove = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 const blank = "\n".repeat(process.stdout.rows);
+
+const gameFlow = {
+  name: null,
+  pos: null,
+  steps: {
+    askName: false
+  }
+};
 
 let SERVER_ADDRESS = "127.0.0.1";
 let SERVER_PORT = 1337;
@@ -17,14 +29,21 @@ client.connect(
   SERVER_ADDRESS,
   function() {
     console.log("Connected");
-    chooseTheMove();
+    // chooseTheMove();
   }
 );
 
 function chooseTheMove() {
-  readMove.question(`\nWhat's your move : `, move => {
-    handleTheMove(move);
-  });
+  const { steps, name } = gameFlow;
+  if (!steps.askName) {
+    readMove.question(`\nWhat's your name ( ${name} ) : `, playerName => {
+      handleNameChange(playerName);
+    });
+  } else {
+    readMove.question(`\nWhat's your move : `, move => {
+      handleTheMove(move);
+    });
+  }
 }
 
 function handleTheMove(data) {
@@ -38,10 +57,38 @@ function handleTheMove(data) {
   }
 }
 
+function handleNameChange(playerName) {
+  if (playerName) {
+    gameFlow.name = playerName;
+  }
+  const { name, pos } = gameFlow;
+  client.write(
+    JSON.stringify({
+      payload: {
+        name,
+        pos
+      },
+      type: NAME_CHANGE
+    })
+  );
+  console.log(`\nYour game name : ${name}`);
+}
+
 client.on("data", function(data) {
-  const strData = data.toString();
-  console.log(strData);
-  chooseTheMove();
+  const parsedData = JSON.parse(data.toString());
+  const { type, payload } = parsedData;
+  if (type === START_GAME) {
+    gameFlow.name = `Player ${payload.player}`;
+    gameFlow.pos = payload.player;
+    chooseTheMove();
+  }
+  if (type === GAME_MESSAGE) {
+    console.log(`\n${payload.message}`);
+  }
+  if (type === "move") {
+    console.log(payload.move);
+    chooseTheMove();
+  }
 });
 
 client.on("close", function() {
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var net = require("net");
 
+/* Constants */
+const NAME_CHANGE = "NAME_CHANGE";
+const START_GAME = "START_GAME";
+const GAME_MESSAGE = "GAME_MESSAGE";
+
 const blank = "\n".repeat(process.stdout.rows);
 const movesArr = {
   "1": "1",
@@ -17,51 +22,115 @@ const connectedSockets = new Set();
 const clients = [];
 
 const broadcastData = function(data, client) {
-  for (let sock of this) {
-    if (sock === client) {
-      sock.write(data);
-    }
-  }
+  client.write(data);
 };
 
 var server = net.createServer(function(socket) {
   const clientsSize = clients.length;
-  console.log(clientsSize);
   if (clientsSize === 2) {
     socket.end("Maximum connections");
   } else {
     console.log("new client connected");
-    // connectedSockets.add(socket);
+    const playerPos = clientsSize + 1;
     clients.push({
-      name: null,
+      name: `Player ${playerPos}`,
       socket,
       moves: [],
-      pos: clientsSize + 1
+      pos: playerPos,
+      nameChanged: false
     });
+    broadcastData(
+      JSON.stringify({
+        payload: {
+          player: playerPos
+        },
+        type: START_GAME
+      }),
+      socket
+    );
 
     socket.on("end", function() {
       console.log("Connection ended");
       const player = getClientIndex(clients, socket);
-      // connectedSockets.delete(socket);
       clients.splice(player - 1, 1);
     });
 
     socket.on("error", function(error) {
       console.log("Connection error");
       const player = getClientIndex(clients, socket);
-      // connectedSockets.delete(socket);
       clients.splice(player - 1, 1);
     });
 
     socket.on("data", function(data) {
-      const strData = data.toString();
-      console.log(strData);
-      const player = getClientIndex(clients, socket);
-      console.log(`Player ${player}`);
-      broadcastData(strData, socket);
+      const parsedData = JSON.parse(data.toString());
+      const { type, payload } = parsedData;
+      if (type === NAME_CHANGE) {
+        handleNameChange(payload);
+      } else {
+        const strData = data.toString();
+        console.log(strData);
+        const player = getClientIndex(clients, socket);
+        console.log(`Player ${player}`);
+        broadcastData(
+          JSON.stringify({
+            payload: {
+              move: strData
+            },
+            type: START_GAME
+          }),
+          socket
+        );
+      }
     });
   }
 });
+
+function handleNameChange(data) {
+  const { name, pos } = data;
+  clients[pos - 1].name = name;
+  clients[pos - 1].nameChanged = true;
+  console.log(`\nPlayer ${pos} changed name to : ${name}`);
+  checkUserStatusAndStartGame();
+}
+
+function checkUserStatusAndStartGame() {
+  if (clients.length === 1) {
+    broadcastData(
+      JSON.stringify({
+        payload: {
+          message: "Waiting for Player 2 to connect..."
+        },
+        type: GAME_MESSAGE
+      }),
+      clients[0].socket
+    );
+  }
+  if (clients.length === 2) {
+    const checkNameChange = clients.filter(cli => cli.nameChanged === false);
+    if (checkNameChange.length === 0) {
+      startMainGame();
+    }
+    if (checkNameChange.length === 1) {
+      let eligPlayer = clients[0].socket;
+      if (checkNameChange.pos === 1) {
+        eligPlayer = clients[1].socket;
+      }
+      broadcastData(
+        JSON.stringify({
+          payload: {
+            message: `Waiting for Player ${checkNameChange.pos} for name change`
+          },
+          type: GAME_MESSAGE
+        }),
+        eligPlayer
+      );
+    }
+  }
+}
+
+function startMainGame() {
+  console.log("Start the main game man");
+}
 
 function getClientIndex(clients, sock) {
   var client = clients.find(cliSock => cliSock.socket === sock);
